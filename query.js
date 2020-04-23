ad3abf37c0762d3fae009e2ec9b14f012fd36c69
Chloe <chloeh24@hotmail.com>
2020-04-23 15:09:41 +0100

    add query function relates #8
    Co-authored-by: vatskan <vatskan@gmail.com>

--- query.js
+++ query.js
@@ -1,1 +1,17 @@
-f;
+function query(url, options) {
+	return fetch(url, options).then((res) => {
+		if (!res.ok) {
+			const error = new Error("Not a valid HTTP request");
+			error.status = res.status;
+			throw error;
+		}
+		const contentType = res.headers.get("content-type");
+		if (contentType && contentType.includes("json")) {
+			return res.json();
+		} else {
+			return res.text();
+		}
+	});
+}
+
+export default query;
