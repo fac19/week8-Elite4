451744b7a37da87433bef467f0488b960f6b7014
Joe Jackson <joe.m.jackson@live.co.uk>
2020-04-23 16:13:55 +0100

    Added a comment in home.js Relates #7
    Co-authored-by: vatskan <vatskan@gmail.com>

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,8 @@
+// const appContainer = document.querySelector("#app");
+// const loginNav = document.querySelector("#loggedInNav");
+// const domFrag = loginNav.content.cloneNode(true);
+// appContainer.appendChild(domFrag);
+
 function home() {
     console.log("you're home!")
     app.innerHTML =`<h1>home<\h1>`  
