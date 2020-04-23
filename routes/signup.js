3796288f4845063274cc2bf3cb83718753e3d14d
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-04-23 13:02:58 +0100

    Add more to signup handler Relates #7
    Co-authored-by: @joepock123 <joe.m.jackson@live.co.uk>

--- routes/signup.js
+++ routes/signup.js
@@ -1,35 +1,52 @@
-// import query from "../query.js"
+import query from "../query.js";
 
 const app = document.querySelector("#app");
 
 const html = /*html*/ `
-<form>
-    <label for="name">Name</label>
-    <input type="text" id="name" 
-    placeholder="Insert name here" required>
+<form class="signup-form">
+  <label for="name">Name</label>
+  <input type="text" id="name" 
+  placeholder="Insert name here" 
+  name="name" required>
 
-    <label for="email">Email</label>
-    <input type="email" id="email" 
-    placeholder="Insert email here" required>
+  <label for="email">Email</label>
+  <input type="email" id="email" 
+  placeholder="Insert email here" 
+  name="email" required>
 
-    <label for="password">Password</label>
-    <input type="password" id="password" 
-    placeholder="Insert password here" required>
-    
-    <div id="errorMessage"></div>
+  <label for="password">Password</label>
+  <input type="password" id="password" 
+  placeholder="Insert password here" 
+  name="password" required>
+  
+  <div id="errorMessage"></div>
 
-    <button type="submit">Sign up</button>
+  <button type="submit">Sign up</button>
 </form>
 `;
 
 function signUp () {
-    // Insert our lovely HTML
-    console.log("running signUp")
-    app.innerHTML = html;
-    
-    // Make a fetch POST request
+  app.innerHTML = html;
 
-    // Do some shit with the response
+  app.querySelector(".signup-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formObject = Object.fromEntries(formData);
+
+    // query("https://fac19-pokemon.herokuapp.com/v1/users", {
+    //   method: "POST",
+    //   body: JSON.stringify(formObject)
+    // })
+    //   .then((body) => {
+    //     window.localStorage.setItem("token", body.access_token);
+    //     window.localStorage.setItem("userId", body.id);
+    //     redirect("/");
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     app.querySelector("#message").append("Something went wrong :(");
+    //   });
+  })
 }
 
 export default signUp;
